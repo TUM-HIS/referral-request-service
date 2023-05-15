6f5a96ab81898d684a557baa84351256ec98703f
samAricha <samaricha001.com>
2023-05-15 23:50:39 +0300

    setup curl for post request

--- app/utils/ReferralRequestService.php
+++ app/utils/ReferralRequestService.php
@@ -10,7 +10,47 @@ class ReferralRequestService
 
         $curl = curl_init();
 
+        $url = 'http://example.com/api/endpoint';
 
+
+        $headers = [
+            'Content-Type: application/json',
+            'Authorization: Bearer {your_token}',
+        ];
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+
+
+        $data = [
+            'name' => 'John Doe',
+            'email' => 'johndoe@example.com',
+        ];
+
+        curl_setopt($curl, CURLOPT_POST, 1);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($data));
+
+        curl_setopt($curl, CURLOPT_URL, $url);
+
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        $response = curl_exec($curl);
+
+        if ($response === false) {
+            $error = curl_error($curl);
+            // Handle the error
+        } else {
+            // Process the response
+            // $response contains the response body
+
+            echo $response;
+
+            $info = curl_getinfo($curl);
+
+            // Display the response details
+            echo "HTTP Code: " . $info['http_code'] . "\n";
+            echo "Response Body: " . $response . "\n";
+        }
+
+        // Close the cURL handle
+        curl_close($curl);
 
 
 
