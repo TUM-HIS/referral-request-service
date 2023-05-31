c53bf3f99e377ef1f4db8bb90591cced8e99c192
samAricha <samaricha001.com>
2023-05-31 09:31:10 +0300

    referral request seeder added

--- database/seeders/ReferralRequestSeeder.php
+++ database/seeders/ReferralRequestSeeder.php
@@ -2,16 +2,15 @@
 
 namespace Database\Seeders;
 
+use App\Models\ReferralRequest;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class ReferralRequestSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
-    public function run(): void
+    public function run()
     {
-        //
+        // Generate 10 dummy service requests using the factory
+        ReferralRequest::factory()->count(10)->create();
     }
 }
